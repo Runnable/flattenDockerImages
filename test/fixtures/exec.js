f8963630a538ee3c491dd02916a05035028579a4
generalhenry <htilford@gmail.com>
2013-10-14 17:31:24 -0700

    silience

--- test/fixtures/exec.js
+++ test/fixtures/exec.js
@@ -1,6 +1,6 @@
 require('child_process').exec = exec;
 
 function exec (cmd, cb) {
-  console.log('command:', cmd);
+  // console.log('command:', cmd);
   cb(null, 'Successfully built foo');
 } 
--- test/fixtures/fs.js
+++ test/fixtures/fs.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
 fs.readdir = function (dir, cb) {
-  console.log('dir:', dir);
+  // console.log('dir:', dir);
   if (dir === '/var/lib/docker/graph/5/layer/') {
     cb(null, [
       'index.php',
@@ -35,7 +35,7 @@ fs.readdir = function (dir, cb) {
 };
 
 fs.stat = function (dir, cb) {
-  console.log('stat:', dir);
+  // console.log('stat:', dir);
   cb(null, {
     isDirectory: function () {
       if (/Dir$/.test(dir)) {
@@ -48,11 +48,18 @@ fs.stat = function (dir, cb) {
 };
 
 fs.exists = function (file, cb) {
-  console.log('exists:', file);
-  cb(true);
+  // console.log('exists:', file);
+  var exists = true;
+  if (/\/index.html/.test(file)) {
+    exists = false;
+  } 
+  if (/\/\.wh\.aufs/.test(file)) {
+    exists = false;
+  }
+  cb(exists);
 };
 
 fs.writeFile = function (file, data, cb) {
-  console.log('write:', file, data);
+  // console.log('write:', file, data);
   cb();
 };
