c9c9406616556061c98aa7db0b97d44a03df5a35
generalhenry <htilford@gmail.com>
2013-10-21 15:31:47 -0700

    rollbar

--- test/docklet.js
+++ test/docklet.js
@@ -5,6 +5,6 @@ var flattenImage = require('../');
 
 describe('docklet interface', function () {
   it('should flatten an image', function (done) {
-    flattenImage('beep', 'boop', 5, done);
+    flattenImage('beep', 'boop', 5, {}, done);
   });
 });
